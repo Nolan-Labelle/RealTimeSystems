b2e1d0efde71d6cd10b7c3fcafebdf26127c5e76
Ty-ArmDev <gauvreat@myumanitoba.ca>
2017-03-24 17:54:19 -0500

    Queues work, single and double press working.

--- Source/main.c
+++ Source/main.c
@@ -10,8 +10,8 @@
 #include "stm32f4xx_exti.h"
 
 #define STACK_SIZE_MIN	128	/* usStackDepth	- the stack size DEFINED IN WORDS.*/
-#define LED_OFF 0
-#define LED_ON  1
+#define LED_OFF 0x00000000
+#define LED_ON  0x00000001
 
 typedef struct
 {
@@ -24,103 +24,112 @@ typedef struct
 //Globals:
 static int clicks = 0;
 static int whichLED = LED_BLUE;
+static TimerHandle_t clickTimer;
+static int lastPress = 0;
+static int debounceDelay = 125 / portTICK_RATE_MS;
+static TickType_t thisPress;
 static QueueHandle_t xBlueQueue;
 static QueueHandle_t xGreenQueue;
 static QueueHandle_t xOrangeQueue;
 static QueueHandle_t xRedQueue;
-static TimerHandle_t clickTimer;
-static int lastPress = 0;
-static int debounceDelay = 500 / portTICK_RATE_MS;
-static TickType_t thisPress;
-static int p = 0;
+static int* blueCmd = 0;
+static int* greenCmd = 0;
+static int* orangeCmd = 0;
+static int* redCmd = 0;
+static int* offCmd;
+static int* onCmd;
 
 void panic() //If something has gone horribly wrong, then disco mode.
 {
-	for(;;)
-	{
-		STM_EVAL_LEDToggle(LED_BLUE);
-		STM_EVAL_LEDToggle(LED_GREEN);
-		STM_EVAL_LEDToggle(LED_ORANGE);
-		STM_EVAL_LEDToggle(LED_RED);
-		vTaskDelay(250 / portTICK_RATE_MS);
-	}
+	STM_EVAL_LEDOn(LED_BLUE);
+	STM_EVAL_LEDOn(LED_GREEN);
+	STM_EVAL_LEDOn(LED_ORANGE);
+	STM_EVAL_LEDOn(LED_RED);
 }
 
 void vBlueCoffee(void *pvParameters)
-{
-	int sentCommand;
-	
+{	
 	for(;;)
 	{
-		while(xQueueReceive(xBlueQueue, &sentCommand, 0))
+		while(xQueueReceive(xBlueQueue, blueCmd, 10))
 		{
-			if (sentCommand == LED_OFF)
+			if (*blueCmd == LED_OFF)
 			{
 				STM_EVAL_LEDOff(LED_BLUE);
 			}
-			else if (sentCommand == LED_ON)
+			else if (*blueCmd == LED_ON)
 			{
 				STM_EVAL_LEDOn(LED_BLUE);
+			}
+			else
+			{
+				panic();
 			}
 		}
 	}
 }
 
 void vGreenCoffee(void *pvParameters)
-{
-	int sentCommand;
-	
+{	
 	for(;;)
 	{
-		while(xQueueReceive(xGreenQueue, &sentCommand, 0))
+		while(xQueueReceive(xGreenQueue, greenCmd, 10))
 		{
-			if (sentCommand == LED_OFF)
+			if (*greenCmd == LED_OFF)
 			{
 				STM_EVAL_LEDOff(LED_GREEN);
 			}
-			else if (sentCommand == LED_ON)
+			else if (*greenCmd == LED_ON)
 			{
 				STM_EVAL_LEDOn(LED_GREEN);
+			}
+			else
+			{
+				panic();
 			}
 		}
 	}
 }
 
 void vOrangeCoffee(void *pvParameters)
-{
-	int sentCommand;
-	
+{	
 	for(;;)
 	{
-		while(xQueueReceive(xOrangeQueue, &sentCommand, 0))
+		while(xQueueReceive(xOrangeQueue, orangeCmd, 10))
 		{
-			if (sentCommand == LED_OFF)
+			if (*orangeCmd == LED_OFF)
 			{
 				STM_EVAL_LEDOff(LED_ORANGE);
 			}
-			else if (sentCommand == LED_ON)
+			else if (*orangeCmd == LED_ON)
 			{
 				STM_EVAL_LEDOn(LED_ORANGE);
+			}
+			else
+			{
+				panic();
 			}
 		}
 	}
 }
 
 void vRedCoffee(void *pvParameters)
-{
-	int sentCommand;
-	
+{	
 	for(;;)
 	{
-		while(xQueueReceive(xRedQueue, &sentCommand, 0))
+		while(xQueueReceive(xRedQueue, redCmd, 10))
 		{
-			if (sentCommand == LED_OFF)
+			if (*redCmd == LED_OFF)
 			{
 				STM_EVAL_LEDOff(LED_RED);
 			}
-			else if (sentCommand == LED_ON)
+			else if (*redCmd == LED_ON)
 			{
 				STM_EVAL_LEDOn(LED_RED);
+			}
+			else
+			{
+				panic();
 			}
 		}
 	}
@@ -130,55 +139,140 @@ void singleClick()
 {
 	//move LED around in a circle, send messages to all 4 color queues
 	//will probably break later when we need multiple LED's on/blinking at once.
+	int fail = 0;
+	
 	switch (whichLED)
 	{
 		case LED_BLUE:
-			whichLED = LED_GREEN;
-			xQueueSendFromISR(xBlueQueue, 	LED_OFF, NULL);
-			xQueueSendFromISR(xGreenQueue, 	(void*)LED_ON, NULL);
-			xQueueSendFromISR(xOrangeQueue, LED_OFF, NULL);
-			xQueueSendFromISR(xRedQueue, 	LED_OFF, NULL);
+			whichLED = 	LED_GREEN;
+			if(!xQueueSend(xBlueQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xGreenQueue, onCmd, 0))
+				fail++;
+			if(!xQueueSend(xOrangeQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xRedQueue, offCmd, 0))
+				fail++;
 			break;
 		case LED_GREEN:
-			whichLED = LED_ORANGE;
-			xQueueSendFromISR(xBlueQueue, 	LED_OFF, NULL);
-			xQueueSendFromISR(xGreenQueue, 	LED_OFF, NULL);
-			xQueueSendFromISR(xOrangeQueue, (void*)LED_ON, NULL);
-			xQueueSendFromISR(xRedQueue, 	LED_OFF, NULL);
+			whichLED = 	LED_ORANGE;
+			if(!xQueueSend(xBlueQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xGreenQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xOrangeQueue, onCmd, 0))
+				fail++;
+			if(!xQueueSend(xRedQueue, offCmd, 0))
+				fail++;
 			break;
 		case LED_ORANGE:
-			whichLED = LED_RED;
-			xQueueSendFromISR(xBlueQueue, 	LED_OFF, NULL);
-			xQueueSendFromISR(xGreenQueue, 	LED_OFF, NULL);
-			xQueueSendFromISR(xOrangeQueue, LED_OFF, NULL);
-			xQueueSendFromISR(xRedQueue, 	(void*)LED_ON, NULL);
+			whichLED = 	LED_RED;
+			if(!xQueueSend(xBlueQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xGreenQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xOrangeQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xRedQueue, onCmd, 0))
+				fail++;
 			break;
 		case LED_RED:
-			whichLED = LED_BLUE;
-			xQueueSendFromISR(xBlueQueue, 	(void*)LED_ON,  NULL);
-			xQueueSendFromISR(xGreenQueue, 	LED_OFF, NULL);
-			xQueueSendFromISR(xOrangeQueue, LED_OFF, NULL);
-			xQueueSendFromISR(xRedQueue, 	LED_OFF, NULL);
+			whichLED = 	LED_BLUE;
+			if(!xQueueSend(xBlueQueue, onCmd, 0))
+				fail++;
+			if(!xQueueSend(xGreenQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xOrangeQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xRedQueue, offCmd, 0))
+				fail++;
 			break;
 		default:
 			panic();
+			break;
 	}
+	if (fail)
+	{
+		panic();
+	}
+	clicks = 0;
 }
 
 void doubleClick()
 {
-	//nothing yet.
+	int fail = 0;
+	
+	switch (whichLED)
+	{
+		case LED_BLUE:
+			whichLED = 	LED_RED;
+			if(!xQueueSend(xBlueQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xGreenQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xOrangeQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xRedQueue, onCmd, 0))
+				fail++;
+			break;
+		case LED_GREEN:
+			whichLED = 	LED_BLUE;
+			if(!xQueueSend(xBlueQueue, onCmd, 0))
+				fail++;
+			if(!xQueueSend(xGreenQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xOrangeQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xRedQueue, offCmd, 0))
+				fail++;
+			break;
+		case LED_ORANGE:
+			whichLED = 	LED_GREEN;
+			if(!xQueueSend(xBlueQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xGreenQueue, onCmd, 0))
+				fail++;
+			if(!xQueueSend(xOrangeQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xRedQueue, offCmd, 0))
+				fail++;
+			break;
+		case LED_RED:
+			whichLED = 	LED_ORANGE;
+			if(!xQueueSend(xBlueQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xGreenQueue, offCmd, 0))
+				fail++;
+			if(!xQueueSend(xOrangeQueue, onCmd, 0))
+				fail++;
+			if(!xQueueSend(xRedQueue, offCmd, 0))
+				fail++;
+			break;
+		default:
+			panic();
+			break;
+	}
+	if (fail)
+	{
+		panic();
+	}
+	clicks = 0;
 }
 
 void vClickTimerHandler(TimerHandle_t xTimer)
 {
-	STM_EVAL_LEDToggle(LED_RED);
 	if (clicks == 1)
+	{
 		singleClick();
-	if (clicks >= 2)
+	}
+	else if (clicks >= 2)
+	{
 		doubleClick();
-	if (clicks <= 0)
+	}
+	else
+	{
 		panic();
+	}
 }
 
 void EXTI0_IRQHandler()
@@ -190,21 +284,20 @@ void EXTI0_IRQHandler()
 	{
 		if (clicks == 0)
 		{
+			clicks++;
 			if(xTimerStartFromISR(clickTimer, &xHigherPriorityTaskWoken) != pdPASS)
 			{
 				//Timer was not able to start
+				panic();
 			}
 		}
-		clicks++;
+		else
+		{
+			clicks++;
+		}
 	}
 	lastPress = thisPress;
-	
-	if (xHigherPriorityTaskWoken == pdTRUE)
-	{
-		STM_EVAL_LEDToggle(LED_ORANGE);
-		//portYIELD_FROM_ISR(1);
-		EXTI_ClearITPendingBit(EXTI_Line0); //Holy fuck that took all night.
-	}
+	EXTI_ClearITPendingBit(EXTI_Line0);
 }
 
 void vSelector(void *pvParameters)
@@ -216,8 +309,15 @@ void vSelector(void *pvParameters)
 }
 
 int main(void)
-{
-	SelectorPack* selectorPack = (SelectorPack*)pvPortMalloc(sizeof(SelectorPack));
+{	
+	blueCmd =   (int*)pvPortMalloc(sizeof(int));
+	greenCmd =  (int*)pvPortMalloc(sizeof(int));
+	orangeCmd = (int*)pvPortMalloc(sizeof(int));
+	redCmd = 	(int*)pvPortMalloc(sizeof(int));
+	offCmd = 	(int*)pvPortMalloc(sizeof(int));
+	onCmd = 	(int*)pvPortMalloc(sizeof(int));
+	*offCmd = 0;
+	*onCmd = 1;
 	
 	NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 ); //Enable preemption. Must happen before scheduler.
 	
@@ -227,23 +327,19 @@ int main(void)
 	STM_EVAL_LEDInit(LED_RED);
 	STM_EVAL_PBInit(BUTTON_USER, BUTTON_MODE_EXTI);
 	
-	xBlueQueue = 	xQueueCreate(5, sizeof(int));
-	xGreenQueue = 	xQueueCreate(5, sizeof(int));
-	xOrangeQueue = 	xQueueCreate(5, sizeof(int));
-	xRedQueue = 	xQueueCreate(5, sizeof(int));
-	
-	STM_EVAL_LEDOn(LED_BLUE); //Start with the blue LED on
-	STM_EVAL_LEDOff(LED_GREEN);
-	STM_EVAL_LEDOff(LED_ORANGE);
-	STM_EVAL_LEDOff(LED_RED);
+	xBlueQueue 		= xQueueCreate(5, sizeof(int));
+	xGreenQueue 	= xQueueCreate(5, sizeof(int));
+	xOrangeQueue 	= xQueueCreate(5, sizeof(int));
+	xRedQueue 		= xQueueCreate(5, sizeof(int));
 		
-	xTaskCreate( vBlueCoffee, 	"Blue coffee", 		STACK_SIZE_MIN, (void*)1000, 	 1, NULL );
-	xTaskCreate( vGreenCoffee, 	"Green coffee", 	STACK_SIZE_MIN, (void*)2000, 	 1, NULL );
-	xTaskCreate( vOrangeCoffee, "Orange coffee", 	STACK_SIZE_MIN, (void*)4000, 	 1, NULL );
-	xTaskCreate( vRedCoffee, 	"Red coffee", 		STACK_SIZE_MIN, (void*)8000, 	 1, NULL );
-	xTaskCreate( vSelector, 	"Selector", 		STACK_SIZE_MIN, (void*)selectorPack, 1, NULL );
+	xTaskCreate( vBlueCoffee, 	"Blue coffee", 		STACK_SIZE_MIN, (void*)1000, 1, NULL );
+	xTaskCreate( vGreenCoffee, 	"Green coffee", 	STACK_SIZE_MIN, (void*)2000, 1, NULL );
+	xTaskCreate( vOrangeCoffee, "Orange coffee", 	STACK_SIZE_MIN, (void*)4000, 1, NULL );
+	xTaskCreate( vRedCoffee, 	"Red coffee", 		STACK_SIZE_MIN, (void*)8000, 1, NULL );
+	xTaskCreate( vSelector, 	"Selector", 		STACK_SIZE_MIN, (void*)0, 	 1, NULL );
 	
-	clickTimer = xTimerCreate("Double click timer", pdMS_TO_TICKS(500), pdFALSE, (void*)0, vClickTimerHandler);
+	clickTimer = xTimerCreate("Double click timer", pdMS_TO_TICKS(750), pdFALSE, (void*)0, vClickTimerHandler);
+	STM_EVAL_LEDOn(LED_BLUE);
 	
 	vTaskStartScheduler();
 	panic();
